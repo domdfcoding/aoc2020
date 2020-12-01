3d0ae11f3cb0f9507b1fa412f97dd64359311501
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-12-01 18:34:35 +0000

    Day 1 complete.

---
+++ 01/code.py
@@ -0,0 +1,89 @@
+"""
+--- Day 1: Report Repair ---
+
+After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island.
+Surely, Christmas will go on without you.
+
+The tropical island has its own currency and is entirely cash-only.
+The gold coins used there have a little picture of a starfish; the locals just call them stars.
+None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins
+by the time you arrive so you can pay the deposit on your room.
+
+To save your vacation, you need to get all fifty stars by December 25th.
+
+Collect stars by solving puzzles.
+Two puzzles will be made available on each day in the Advent calendar;
+the second puzzle is unlocked when you complete the first.
+Each puzzle grants one star. Good luck!
+
+Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input);
+apparently, something isn't quite adding up.
+
+Specifically, they need you to find the two entries that sum to ``2020`` and then multiply those two numbers together.
+
+For example, suppose your expense report contained the following:
+
+.. code-block::
+
+	1721
+	979
+	366
+	299
+	675
+	1456
+
+In this list, the two entries that sum to ``2020`` are ``1721`` and ``299``.
+Multiplying them together produces ``1721 * 299 = 514579``, so the correct answer is ``514579``.
+
+Of course, your expense report is much larger. #
+Find the two entries that sum to ``2020``; what do you get if you multiply them together?
+"""
+
+import operator
+from domdf_python_tools.paths import PathPlus
+import itertools
+
+
+# ==========================
+print("Part One")
+# ==========================
+
+expense_report = sorted(int(x) for x in PathPlus("input.txt").read_lines() if x)
+
+for pair in itertools.permutations(expense_report, 2):
+	if operator.add(*pair) == 2020:
+		print("The pair of numbers is:", pair)
+		print("The answer is:", operator.mul(*pair))
+		break
+
+# ==========================
+print("\nPart Two")
+# ==========================
+
+"""
+The Elves in accounting are thankful for your help; 
+one of them even offers you a starfish coin they had left over from a past vacation. 
+They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. 
+Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum to 2020?
+"""
+
+
+def tri_add(a, b, c):
+	return a + b + c
+
+
+def tri_mul(a, b, c):
+	return a * b * c
+
+
+for pair in itertools.permutations(expense_report, 3):
+	if tri_add(*pair) == 2020:
+		print("The three numbers are:", pair)
+		print("The answer is:", tri_mul(*pair))
+		break
+
+
