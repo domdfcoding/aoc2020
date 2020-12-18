b8eb09da12a5374c89d550b8408a218aaa7c383e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-12-18 09:54:29 +0000

    Added Day 17 Part 2 solution.

--- 17/code.py
+++ 17/code.py
@@ -246,7 +246,7 @@ for cycle in range(6):
 							if check_coord == (x, y, z):
 								continue
 
-							if (x + x_offset, y + y_offset, z + z_offset) in active_cubes:
+							if check_coord in active_cubes:
 								active_neighbours += 1
 
 				value = state[(x, y, z)]
@@ -266,5 +266,350 @@ print(f"After 6 cycles there are {Counter(state.values())['#']} active cubes")  
 print("\nPart Two")
 # ==========================
 """
+For some reason, your simulated results don't match what the experimental energy source engineers expected.
+Apparently, the pocket dimension actually has four spatial dimensions, not three.
 
+The pocket dimension contains an infinite 4-dimensional grid.
+At every integer 4-dimensional coordinate (``x,y,z,w``), there exists a single cube (really, a hypercube)
+which is still either active or inactive.
+
+Each cube only ever considers its neighbors:
+any of the 80 other cubes where any of their coordinates differ by at most 1.
+For example, given the cube at ``x=1,y=2,z=3,w=4``,
+its neighbors include the cube at ``x=2,y=2,z=3,w=3``, the cube at ``x=0,y=2,z=3,w=4``, and so on.
+
+The initial state of the pocket dimension still consists of a small flat region of cubes.
+Furthermore, the same rules for cycle updating still apply:
+during each cycle, consider the number of active neighbors of each cube.
+
+For example, consider the same initial state as in the example above.
+Even though the pocket dimension is 4-dimensional, this initial state represents a small 2-dimensional slice of it.
+(In particular, this initial state defines a 3x3x1x1 region of the 4-dimensional space.)
+
+Simulating a few cycles from this initial state produces the following configurations,
+where the result of each cycle is shown layer-by-layer at each given z and w coordinate::
+
+	Before any cycles:
+
+	z=0, w=0
+	.#.
+	..#
+	###
+
+
+	After 1 cycle:
+
+	z=-1, w=-1
+	#..
+	..#
+	.#.
+
+	z=0, w=-1
+	#..
+	..#
+	.#.
+
+	z=1, w=-1
+	#..
+	..#
+	.#.
+
+	z=-1, w=0
+	#..
+	..#
+	.#.
+
+	z=0, w=0
+	#.#
+	.##
+	.#.
+
+	z=1, w=0
+	#..
+	..#
+	.#.
+
+	z=-1, w=1
+	#..
+	..#
+	.#.
+
+	z=0, w=1
+	#..
+	..#
+	.#.
+
+	z=1, w=1
+	#..
+	..#
+	.#.
+
+
+	After 2 cycles:
+
+	z=-2, w=-2
+	.....
+	.....
+	..#..
+	.....
+	.....
+
+	z=-1, w=-2
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=0, w=-2
+	###..
+	##.##
+	#...#
+	.#..#
+	.###.
+
+	z=1, w=-2
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=2, w=-2
+	.....
+	.....
+	..#..
+	.....
+	.....
+
+	z=-2, w=-1
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=-1, w=-1
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=0, w=-1
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=1, w=-1
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=2, w=-1
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=-2, w=0
+	###..
+	##.##
+	#...#
+	.#..#
+	.###.
+
+	z=-1, w=0
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=0, w=0
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=1, w=0
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=2, w=0
+	###..
+	##.##
+	#...#
+	.#..#
+	.###.
+
+	z=-2, w=1
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=-1, w=1
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=0, w=1
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=1, w=1
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=2, w=1
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=-2, w=2
+	.....
+	.....
+	..#..
+	.....
+	.....
+
+	z=-1, w=2
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=0, w=2
+	###..
+	##.##
+	#...#
+	.#..#
+	.###.
+
+	z=1, w=2
+	.....
+	.....
+	.....
+	.....
+	.....
+
+	z=2, w=2
+	.....
+	.....
+	..#..
+	.....
+	.....
+
+After the full six-cycle boot process completes, ``848`` cubes are left in the active state.
+
+Starting with your given initial configuration, simulate six cycles in a 4-dimensional space.
+How many cubes are left in the active state after the sixth cycle?
 """
+
+state: Dict[Tuple[int, int, int, int], str] = defaultdict(lambda: '.')
+
+for y, row in enumerate(initial_state):
+	for x, value in enumerate(row):
+		state[(x, y, 0, 0)] = value
+
+minx = 0
+maxx = len(initial_state[0]) - 1
+miny = 0
+maxy = len(initial_state) - 1
+minz = 0
+maxz = 0
+minw = 0
+maxw = 0
+
+
+def visualise_state():
+	for w in range(minw, maxw + 1):
+		print(f"w={w}")
+
+		for z in range(minz, maxz + 1):
+			print(f"z={z}")
+
+			for y in range(miny, maxy + 1):
+				for x in range(minx, maxx + 1):
+					print(state[(x, y, z, w)], end='')
+
+				print()
+
+			print()
+		print()
+
+
+# visualise_state()
+
+for cycle in range(6):
+	# print(f"\nAfter {cycle + 1} cycles:")
+
+	minw -= 1
+	minz -= 1
+	minx -= 1
+	miny -= 1
+	maxw += 1
+	maxz += 1
+	maxx += 1
+	maxy += 1
+	active_cubes = []
+
+	for w in range(minw, maxw + 1):
+		for z in range(minz, maxz + 1):
+			for y in range(miny, maxy + 1):
+				for x in range(minx, maxx + 1):
+					value = state[(x, y, z, w)]
+					if value == '#':
+						active_cubes.append((x, y, z, w))
+
+	for w in range(minw, maxw + 1):
+		for z in range(minz, maxz + 1):
+			for y in range(miny, maxy + 1):
+				for x in range(minx, maxx + 1):
+					active_neighbours = 0
+
+					for x_offset in range(-1, 2):
+						for y_offset in range(-1, 2):
+							for z_offset in range(-1, 2):
+								for w_offset in range(-1, 2):
+									check_coord = (x + x_offset, y + y_offset, z + z_offset, w + w_offset)
+									if check_coord == (x, y, z, w):
+										continue
+
+									if check_coord in active_cubes:
+										active_neighbours += 1
+
+					value = state[(x, y, z, w)]
+
+					if value == '#':
+						if active_neighbours not in {2, 3}:
+							state[(x, y, z, w)] = '.'
+					else:
+						if active_neighbours == 3:
+							state[(x, y, z, w)] = '#'
+
+	# visualise_state()
+
+print(f"After 6 cycles there are {Counter(state.values())['#']} active cubes")  # 2324
